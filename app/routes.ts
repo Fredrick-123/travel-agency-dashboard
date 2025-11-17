18ba14a3d94d4f3f5f47620e8899fab2ec4b69de
Fredrick-123 <fredricknicholas8@gmail.com>
2025-11-17 20:35:39 +0100

    Routing & extra

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,16 @@
-import {type RouteConfig, route, layout} from "@react-router/dev/routes";
+// import {type RouteConfig, route, layout} from "@react-router/dev/routes";
+//
+// export default [
+//     layout('routes/admin/admin-layout.tsx'),
+//     route("dashboard", "routes/admin/dashboard.tsx"),
+//     route("all-users", "routes/admin/all-users.tsx"),
+// ] satisfies RouteConfig;
+
+import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
-    layout('/routes/admin/admin-layout.tsx'),
-    route("dashboard", "routes/admin/dashboard.tsx"),
-    route("all-users", "routes/admin/all-user.tsx"),
+    layout("routes/admin/admin-layout.tsx", [
+        route("dashboard", "routes/admin/dashboard.tsx"),
+        route("all-users", "routes/admin/all-users.tsx"),
+    ])
 ] satisfies RouteConfig;
